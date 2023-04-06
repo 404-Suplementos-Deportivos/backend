7844de33b913e53b334dec2c47b5f55188507093
FabrizzioLoPresti <fabrizziolopresti1999@gmail.com>
2023-04-06 14:59:01 -0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import configuration from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Habilitar CORS y permitir solo la URL del Frontend
+  app.enableCors({
+    origin: configuration().frontendUrl
+  });
+  await app.listen(4000);
 }
 bootstrap();
