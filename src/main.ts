15d5d6a145240ee2521077b2f3702b77518d46af
FabrizzioLoPresti <fabrizziolopresti1999@gmail.com>
2023-06-09 09:08:28 -0300

    Fixed Railway

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configuration from './config/configuration';
 
+const port = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Habilitar CORS y permitir solo la URL del Frontend
   app.enableCors({
     origin: [configuration().frontendUrl, configuration().frontendUrlAdmin]
   });
-  await app.listen(4000);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
